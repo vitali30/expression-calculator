2fbec3f972e1e6614f9e3fd0d84009f4f29addfc
vitali30 <vysotskivitali@gmail.com>
2020-03-04 11:56:16 +0300

    add file index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
+function eval() {return;}
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let array = [...expr.replace(/\s/g, "")];
+    let n = 0;
+    let m = 0;
+    for (let el of array){
+        if(el =="(")n+=1;
+        else if (el ==')') m +=1; 
+    }
+    if(n !== m) throw new Error('ExpressionError: Brackets must be paired')
+    for (let i =0 ; i < array.length ; i ++){
+        if (array[i] == "/" && array[i+1] == "0") throw new Error('TypeError: Division by zero.')
+    }
+    let result = new Function("return " + expr.replace(/\s/g, "")); 
+    return(result());
 }
-
 module.exports = {
     expressionCalculator
 }
